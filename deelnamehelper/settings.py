4b09d10c821e39917ec079f5d42ce5ae99d15c9e
jozef mantis <wej.bakker@mantisbv.nl>
2022-09-21 18:42:48 +0200

    settings

--- deelnamehelper/settings.py
+++ deelnamehelper/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'beatrix',
     'user',
-]
+    'rest_framework',]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
