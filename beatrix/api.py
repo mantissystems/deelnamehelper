3916dd16c24ef87a1e8c0b0a079c60ffa9d5459d
jozef mantis <wej.bakker@mantisbv.nl>
2022-09-26 10:26:03 +0200

    data

--- beatrix/api.py
+++
@@ -1,8 +0,0 @@
-from person.models import Person
-from rest_framework import viewsets,permissions
-from .serializers import PersoonSerializer
-
-class PersoonViewSet(viewsets.ModelViewSet):
-  queryset= Person.objects.all()
-  permission_classes=[permissions.AllowAny]
-  serializer_class=PersoonSerializer
